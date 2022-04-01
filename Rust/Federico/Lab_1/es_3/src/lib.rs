f279a6b73f0b887d52534fd6d6693a0b31348503
FedericoRinaudi <federicorinaudi@gmail.com>
2022-04-01 16:08:30 +0200

    RUST-FEDERICO: Lab1 es 3 annotate

--- Rust/Federico/Lab_1/es_3/src/lib.rs
+++ Rust/Federico/Lab_1/es_3/src/lib.rs
@@ -1,30 +1,61 @@
 use std::cmp::{max, min};
 
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    let start_x;
-    let start_y;
-    let end_x;
-    let end_y;
+    let mut start_x;
+    let mut start_y;
+    let mut end_x;
+    let mut end_y;
+    let mut discarded: u16 = 0;
     let mut res:Vec<String> = vec![];
     for (i, row) in minefield.iter().enumerate(){
+        let mut new_row: String = String::new();
         for (j, el) in row.as_bytes().iter().enumerate(){
-            if *el == b'*' {
-                start_x = max(0, i-1);
-                start_y = max(0, j-1);
-                end_x = min(minefield.len(), i+1);
-                end_y = min(row.len(), j+1);
+            match *el{
+                b'.'  => {
+                    start_x = max(0, i as i8 - 1) as usize;
+                    start_y = max(0, j as i8 - 1) as usize;
+                    end_x = min(row.len(), i + 2);
+                    end_y = min(minefield.len(), j + 2);
 
-                for x in start_x..end_x{
-                    for y in start_y..end_y{
-                        
+                    let mut count: u8 = 0;
+
+                    //perché per iterare ho una ref di una ref??
+
+                    for r in &minefield[start_x..end_x] {
+                        for c_to_check in &r.as_bytes()[start_y..end_y] {
+                            match c_to_check {
+                                b'*' => { count += 1; }
+                                _ => {}
+                            }
+                        }
+                    }
+                    /*
+                    PERCHE' &*?
+                    PERCHE' IL PRIMO MODO NON COMPILA E IL SECONDO SI? (PER IL FATTO CHE NEL RITORNARE IL VALORE DAL MATCH NON E' POI PIU' IN SCOPE LA STRINGA CHE CREO NELLA CONVERSIONE?)
+                    new_row.push_str( match count {
+                        0 => {"."}
+                        _ => {&*count.to_string()}
+                    });
+                    new_row.push_str(&*count.to_string());*/
+                    match count {
+                        0 => {new_row.push_str(".")}
+                        _ => {new_row.push_str(&*count.to_string())}
                     }
                 }
-
-            }
+                b'*' => {
+                    new_row.push_str("*");
+                }
+                _ => {
+                    discarded+=1;
+                }
+            };
         }
+        res.push(new_row);
     }
-
-    return vec!["aaaa".to_string(), "bbbbbbb".to_string()];
+    if discarded>0{
+        println!("In input matrix there are {} characters different from '.' and '*'...\nThese characters has been discarded, are you sure that the input is correct?", discarded);
+    }
+    return res;
     //unimplemented!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{:#?}\n", minefield);
 }
 
--- Rust/Federico/Lab_1/es_3/src/main.rs
+++ Rust/Federico/Lab_1/es_3/src/main.rs
@@ -9,6 +9,25 @@ let prova: [&str] = [&"*...", &"..*.", &"..*.", &"..*."]; così no!!!! come mai?
 
 fn main(){
     let prova: &[&str] = &["*...", "..*.", "..*.", "..*."];
-    let b = annotate(prova);
-    println!("{}", b.len());
+    print_matrix(prova);
+    println!("\n\n");
+    let b: Vec<String> = annotate(prova);
+    print_string_vec(b);
+}
+
+fn print_matrix (m: &[&str]) {
+    for row in m{
+        println!("\n");
+        for el in row.chars(){
+            print!("{}  ", el);
+        }
+    }
+}
+fn print_string_vec (v: Vec<String>) {
+    for row in v{
+        println!("\n");
+        for el in row.chars(){
+            print!("{}  ", el);
+        }
+    }
 }
