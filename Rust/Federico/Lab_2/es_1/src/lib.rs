5bc423065637732976108a2dd99acbd04b3544c1
FedericoRinaudi <federicorinaudi@gmail.com>
2022-04-13 19:31:22 +0200

    FEDERICO-RUST: Lab2 es1

--- Rust/Federico/Lab_2/es_1/src/lib.rs
+++ Rust/Federico/Lab_2/es_1/src/lib.rs
@@ -1,6 +1,7 @@
 use std::cmp;
 
-pub fn find(array: &[i32], key: i32) -> Option<usize> {
+pub fn find<A: AsRef<[T]> , T:Ord>(array: A, key: T) -> Option<usize>{
+    let array = array.as_ref();
     if array.len() == 0 || key < array[0] || key > array[array.len() - 1] {
         return None;
     }
@@ -16,3 +17,43 @@ pub fn find(array: &[i32], key: i32) -> Option<usize> {
         cmp::Ordering::Equal => Some(middle),
     }
 }
+
+pub fn find_iter<A: AsRef<[T]> , T:Ord>(array: A, key: T) -> Option<usize>{
+    let mut array = array.as_ref();
+    if array.len() == 0 || key < array[0] || key > array[array.len() - 1] {
+        return None;
+    }
+
+    let mut found:bool = false;
+    let mut res:usize = 0;
+
+
+    while !found && array.len()!=0 {
+
+        let middle = array.len()/2;
+
+        match key.cmp(&array[middle]) {
+            cmp::Ordering::Less => {
+                array = &array[..middle];
+            },
+            cmp::Ordering::Greater => {
+                array = &array[middle..];
+                res += middle;
+            }
+            cmp::Ordering::Equal => {
+                res += middle;
+                found = true;
+            },
+        }
+    }
+
+    if found {
+        Some(res)
+    }else{
+        None
+    }
+
+
+}
+
+
