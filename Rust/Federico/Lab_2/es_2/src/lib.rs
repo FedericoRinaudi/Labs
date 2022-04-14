7bf5196e76750e16b84238b4f6e4c35a2001a938
FedericoRinaudi <federicorinaudi@gmail.com>
2022-04-15 01:35:33 +0200

    FEDERICO-RUST: lab2 es_2 missing + and - implementation

--- Rust/Federico/Lab_2/es_2/src/lib.rs
+++ Rust/Federico/Lab_2/es_2/src/lib.rs
@@ -1,15 +1,37 @@
-pub struct Clock;
+
+
+use std::fmt;
+use std::fmt::Formatter;
+
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Clock{
+    minutes: i32,
+}
+
+const MINUTES_PER_HOUR: i32 = 60;
+const MINUTES_PER_DAY: i32 = 60*24;
+
+
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Self{
+            minutes: (((minutes + hours * MINUTES_PER_HOUR) % MINUTES_PER_DAY) + MINUTES_PER_DAY ) % MINUTES_PER_DAY,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock::new(0, minutes+self.minutes)
+    }
+
+
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.minutes/60, self.minutes%60)
     }
 }
+
+
